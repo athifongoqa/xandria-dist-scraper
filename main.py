ecdc8ab23abce39d56745eb0f50a4e03ab07fd28
Athi Fongoqa <fongoqaathi@gmail.com>
2022-10-27 13:20:58 +0200

    feat[breaking change]: adding filters + to queues

--- main.py
+++ main.py
@@ -5,14 +5,23 @@ connection = Redis(db=1)
 starting_url = 'https://scrapeme.live/shop/page/1/' 
  
 connection.rpush('crawling:to_visit', starting_url) 
+
+maximum_items = 5 
  
-while True: 
+while True:
+	# count URLs in visited 
+	visited = connection.scard('crawling:visited') 
+	queued = connection.scard('crawling:queued') 
+	if queued + visited > maximum_items: 
+		print('Exiting! Over maximum') 
+		break
+
 	# timeout after 1 minute 
 	item = connection.blpop('crawling:to_visit', 60) 
 	if item is None: 
 		print('Timeout! No more items to process') 
 		break 
- 
+	
 	url = item[1].decode('utf-8') 
 	print('Pop URL', url) 
-	crawl.delay(url) 
+	crawl.delay(url)
--- tasks.py
+++ tasks.py
@@ -1,16 +1,26 @@
 from celery import Celery
+from redis import Redis 
 import requests 
 from bs4 import BeautifulSoup 
-from urllib.parse import urljoin 
+from urllib.parse import urljoin
  
 app = Celery('tasks', broker_url='redis://127.0.0.1:3001/1') 
+
+connection = Redis(db=1) 
  
 @app.task 
 def crawl(url): 
+	connection.sadd('crawling:queued', url) # add URL to set 
 	html = get_html(url) 
 	soup = BeautifulSoup(html, 'html.parser') 
 	links = extract_links(url, soup) 
-	print(links) 
+	for link in links: 
+		if allow_url_filter(link) and not seen(link): 
+			print('Add URL to visit queue', link) 
+			add_to_visit(link) 
+ 
+	# atomically move a URL from queued to visited 
+	connection.smove('crawling:queued', 'crawling:visited', url) 
  
 def get_html(url): 
 	try: 
@@ -27,3 +37,14 @@ def extract_links(url, soup):
 		for a in soup.find_all('a') 
 		if a.get('href') and not(a.get('rel') and 'nofollow' in a.get('rel')) 
 	}) 
+
+def allow_url_filter(url): 
+	return '/shop/page/' in url and '#' not in url 
+ 
+def seen(url): 
+	return connection.sismember('crawling:visited', url) or connection.sismember('crawling:queued', url) 
+ 
+def add_to_visit(url): 
+	# LPOS command is not available in Redis library 
+	if connection.execute_command('LPOS', 'crawling:to_visit', url) is None: 
+		connection.rpush('crawling:to_visit', url) # add URL to the end of the list 
