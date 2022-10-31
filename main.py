6223031b090a6c5b178a0ff6d1ad2a60c6c8d130
Athi Fongoqa <fongoqaathi@gmail.com>
2022-10-31 19:36:22 +0100

    build: integrate into api endpoint

--- main.py
+++ main.py
@@ -1,27 +1,3 @@
-# Entry point to start queuing URLs
-from tasks import queue_url 
-import repo
-
 from xandria import create_app
 
 app = create_app()
-
-# starting_url = 'https://scrapeme.live/shop/page/1/' 
-
-# maximum_items = 30
- 
-# while True:
-#     total = repo.count_visited() + repo.count_queued()
-#     if total >= maximum_items:
-#         print('Exiting! Over maximum:', total)
-#         break
-
-#     # timeout after 1 minute
-#     item = repo.pop_to_visit_blocking(60)
-#     if item is None:
-#         print('Timeout! No more items to process')
-#         break
-
-#     url = item[1].decode('utf-8')
-#     print('Pop URL', url)
-#     queue_url.delay(url, maximum_items)
--- xandria/__init__.py
+++ xandria/__init__.py
@@ -1,10 +1,34 @@
-from fastapi import FastAPI
+# Entry point to start queuing URLs
+from tasks import queue_url 
+import repo
+from fastapi import FastAPI, Request
 
 def create_app() -> FastAPI:
     app = FastAPI()
 
-    @app.get("/")
-    async def root():
-        return {"message": "Hello World"}
+    maximum_items = 30
+
+    @app.post("/")
+    async def startProcess(req: Request):
+        starting_url = await req.body()
+        repo.add_to_visit(starting_url)
+
+        while True:
+            total = repo.count_visited() + repo.count_queued()
+            if total >= maximum_items:
+                print('Exiting! Over maximum:', total)
+                break
+
+            # timeout after 1 minute
+            item = repo.pop_to_visit_blocking(60)
+            if item is None:
+                print('Timeout! No more items to process')
+                break
+
+            url = item[1].decode('utf-8')
+            print('Pop URL', url)
+            queue_url.delay(url, maximum_items)
+
+        return "Done"
 
     return app
